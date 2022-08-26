4020b024893fb818253dbde6a0a71d8ebde355bc
imadeldine <imadeldine@imadeldines-MacBook-Pro.local>
2022-08-26 08:50:12 +0300

    navbar part

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
-import { Header } from "./components";
+import { Header,Navbar } from "./components";
 function App() {
   return (
     <div className="App">
     <Router>
         <Routes>
-        <Route path="/" exact element={<Register />} />
-        <Route path='/home' exact element={<Header/>}/>
-
+        <Route path="/" exact element={<Register />} /> 
+        <Route path='/home' exact element={<div><Navbar/><Header/></div>}/>
+        
+        
         </Routes>
     </Router>
     </div>
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+// import { AiOutlineBars } from "react-icons/ai";
+// import { RiCloseLine } from "react-icons/ri";
+
+
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+  return (
+    <nav className="navbar container">
+      <menu>
+        <ul
+          className="nav-links"
+          id={showMenu ? "nav-links-mobile" : "nav-links-mobile-hide"}
+        >
+          <li>
+            <a href="#feal">Home</a>
+          </li>
+          <li>
+            <a href="#features">Features</a>
+          </li>
+          <li>
+            <a href="#download">Pages</a>
+          </li>
+          <li>
+            <a href="#subscribe">Screenshots</a>
+          </li>
+          <li>
+          <a href="#subscribe">Pricing</a>
+        </li>
+        <li>
+            <a href="#subscribe">Contact</a>
+          </li>
+          
+        </ul>
+      </menu>
+    
+    </nav>
+  );
+};
+
+export default Navbar;
